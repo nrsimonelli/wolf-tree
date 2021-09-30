1ce05c3dee44edef4d2c5ab6097ae9b557fb021a
nrsimonelli <nrsimonelli@gmail.com>
2021-09-29 21:28:09 -0500

    defined propTypes for toggle component and added fall backs for size & node

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ThemeToggle from './ThemeToggle';
-import { RiMoonClearLine, RiSunFoggyFill } from 'react-icons/ri';
-
-const toLightIcon = <RiSunFoggyFill className='h-6 w-6' />;
-const toDarkIcon = <RiMoonClearLine className='h-6 w-6' />;
 
 const App = () => {
   return (
@@ -11,11 +7,7 @@ const App = () => {
       <div className='text-5xl m-16 font-semibold text-transparent bg-clip-text bg-gradient-to-l from-secondary-400 to-accent-600'>
         WolfTree
       </div>
-      <ThemeToggle
-        toggleStyle='text-white dark:text-black bg-accent-500 hover:bg-accent-600'
-        toDarkMode={toDarkIcon}
-        toLightMode={toLightIcon}
-      />
+      <ThemeToggle toggleStyle='text-white dark:text-black bg-accent-500 hover:bg-accent-600 dark:hover:bg-accent-400' />
     </div>
   );
 };
--- src/ThemeToggle.js
+++ src/ThemeToggle.js
@@ -1,15 +1,15 @@
 import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import { ThemeContext } from './ThemeContext';
+import { RiMoonClearLine, RiSunFoggyFill } from 'react-icons/ri';
 
-const ThemeToggle = ({ toLightMode, toDarkMode, toggleStyle }) => {
+const ThemeToggle = ({
+  size,
+  toLightMode,
+  toDarkMode,
+  toggleStyle,
+}) => {
   const { theme, setTheme } = useContext(ThemeContext);
-
-  if (toLightMode === undefined) {
-    toLightMode = 'light mode';
-  }
-  if (toDarkMode === undefined) {
-    toDarkMode = 'dark mode';
-  }
 
   const nextTheme = () => {
     if (theme === 'dark') {
@@ -19,6 +19,27 @@ const ThemeToggle = ({ toLightMode, toDarkMode, toggleStyle }) => {
     }
   };
 
+  const iconSize = {
+    sm: 'h-4 w-4',
+    md: 'h-6 w-6',
+    lg: 'h-8 w-8',
+    xl: 'h-10 w-10',
+  };
+
+  if (!iconSize[size]) {
+    size = 'md';
+  }
+
+  const lightModeIcon = <RiSunFoggyFill className={iconSize[size]} />;
+  const darkModeIcon = <RiMoonClearLine className={iconSize[size]} />;
+
+  if (!toLightMode) {
+    toLightMode = lightModeIcon;
+  }
+  if (!toDarkMode) {
+    toDarkMode = darkModeIcon;
+  }
+
   return (
     <div
       className={`btn transition-all duration-500 ease-in-out ${toggleStyle}`}
@@ -27,6 +48,13 @@ const ThemeToggle = ({ toLightMode, toDarkMode, toggleStyle }) => {
       {theme === 'dark' ? toLightMode : toDarkMode}
     </div>
   );
+};
+
+ThemeToggle.propTypes = {
+  size: PropTypes.oneOf(['sm', 'md', 'lg', 'xl']),
+  toLightMode: PropTypes.node,
+  toDarkMode: PropTypes.node,
+  toggleStyle: PropTypes.string,
 };
 
 export default ThemeToggle;
